d2eab7022d0bd2495c3eaa82a493f31c5693f7d5
Jean-Pierre Gattuso <gattuso@obs-vlfr.fr>
2011-05-25 00:00:00 +0000

    version 2.4.2

--- R/Om.R
+++ R/Om.R
@@ -15,7 +15,7 @@
 c <- carb(flag=flag, var1=var1, var2=var2, S=35, T=25, P=0, Pt=0, Sit=0, k1k2= k1k2, kf= kf, ks=ks,  pHscale= pHscale)
 
 S=35
-perMg <- x*100
+x <- x*100  #conversion to percentage
 
 #Activity coefficients Millero and Pierrot 1998
 gCO3 <- 0.037
@@ -33,7 +33,7 @@ aCO3 <- gCO3*CO3
 ## to find the good IAP (equations derived from Bishoff, 1993)
 IAPbiogenic <- 10^(-8.45022726020772 + -0.0363899891218405*x^1 + 0.0107168547236804*x^2 + -0.000307011230719308*x^3)
 IAPcleaned <- 10^(-8.34594750694325 + -0.0492826022095371*x^1 + 0.0125595815829352*x^2 + -0.0010724925092009*x^3 + 4.33859823603212e-05*x^4 + -6.69727692992637e-07*x^5)
-x <- perMg/100
+x <- x/100 # reconversion to proportion between 0 and 1
 omegaBiogenic <- (aMg^x)*(aCa^(1-x))*(aCO3)/IAPbiogenic
 omegaCleaned <- (aMg^x)*(aCa^(1-x))*(aCO3)/IAPcleaned
 RES <- list(OmegaMgCa_biogenic=omegaBiogenic, OmegaMgCa_biogenic_cleaned=omegaCleaned)
